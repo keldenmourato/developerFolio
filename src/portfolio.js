00b666087c04a2b980d23a665f466a97921c7e2b
Kelden Mourato <121355275+keldenmourato@users.noreply.github.com>
2026-09-23 10:43:49 +0200

    Certifications

--- src/portfolio.js
+++ src/portfolio.js
@@ -279,11 +279,11 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Scrum Fundamentals Certified",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "",
+      image: require("./assets/images/scrum.png"),
+      imageAlt: "Scrum Certified",
       footerLink: [
         {
           name: "Certification",
@@ -300,11 +300,11 @@ const achievementSection = {
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Six Sigma White Belt Certified",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
+        "",
+      image: require("./assets/images/sixsigma_wb.png"),
+      imageAlt: "Six Sigma White Belt",
       footerLink: [
         {
           name: "View Google Assistant Action",
@@ -314,10 +314,10 @@ const achievementSection = {
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
+      title: "KanBan Certified",
+      subtitle: "",
+      image: require("./assets/images/kanban.png"),
+      imageAlt: "KanBan",
       footerLink: [
         {name: "Certification", url: ""},
         {
@@ -327,7 +327,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
